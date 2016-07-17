a4a6778e01e98126c074c927427def6b5104b3ca
Ghislain B <gbeaulac@gmail.com>
2016-07-17 14:13:50 -0400

    Simplifying Aurelia-Chart Webpack example

--- examples-wp/src/app.js
+++ examples-wp/src/app.js
@@ -1,13 +1,65 @@
-export class App {
-  configureRouter(config, router) {
-    config.title = 'Aurelia';
-    config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',       nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',         nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router',  nav: true, title: 'Child Router' },
-      { route: 'chart',         name: 'chart',        moduleId: 'chart-demo',    nav: true, title: 'Chart Demo' }
-    ]);
+export class App
+{
+    constructor(){
+        this.DynamicDoughnutData = {};
+        this.SimpleLineData = {};
 
-    this.router = router;
-  }
+        this.resetPieData();
+        this.resetLineData();
+    }
+
+    resetPieData() {
+        this.DynamicDoughnutData = {
+            labels: ["Red", "Green", "Yellow" ],
+            datasets: [
+                {
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ]
+                }]
+        };
+    }
+
+    resetLineData() {
+        this.SimpleLineData = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "Healthy People",
+                    backgroundColor: "rgba(220,220,220,0.2)",
+                    borderColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: "Ill People",
+                    backgroundColor: "rgba(151,187,205,0.2)",
+                    borderColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+    }
+
+    addEntry() {
+        this.DynamicDoughnutData.labels.push("New Colour");
+        this.DynamicDoughnutData.datasets[0].data.push(50);
+        this.DynamicDoughnutData.datasets[0].backgroundColor.push("#B4FD5C");
+    };
+
 }
--- examples-wp/src/nav-bar.js
+++
@@ -1,8 +0,0 @@
-import {inject, bindable} from 'aurelia-framework';
-
-export class NavBar {
-  @bindable router = null;
-
-  constructor() {
-  }
-}
